a28208f7bfab59d96c9400d6535951cadd3e588a
mateo vargas <mateo.vargast@udea.edu.co>
2025-09-06 22:48:59 -0500

    Spring initializer

--- README.md
+++
@@ -1,2 +0,0 @@
-# GolManager-Backend
-Backend
---
+++ src/main/java/co/golmanager/gestorweb/config/SecurityWebConfig.java
@@ -0,0 +1,29 @@
+package co.golmanager.gestorweb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+
+//Deshabilita Spring Security para mostrar el html de prueba
+@Configuration
+class SecurityWebConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/*");
+    }
+}
