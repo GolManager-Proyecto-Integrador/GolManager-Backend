507ff8b7ff1992b5a9ba15be13c8f06b811d7a34
mateo vargas <mateo.vargast@udea.edu.co>
2025-09-22 19:57:51 -0500

    FEATURE: Creación endpoint GET referees

--- src/main/java/co/golmanager/gestorweb/config/OpenAPIConfig.java
+++ src/main/java/co/golmanager/gestorweb/config/OpenAPIConfig.java
@@ -1,9 +1,26 @@
 package co.golmanager.gestorweb.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import org.springframework.stereotype.Component;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
 
-@Component
+@Configuration
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "GolManager API",
+                version = "0.1",
+                description = "API for managing soccer tournaments, teams, players, and referees."
+
+        ),
+        servers = @Server(
+                url = "http://localhost:8085",
+                description = "Local server"
+        )
+)
+
 @SecurityScheme(
         name = "Bearer Authentication",
         type = io.swagger.v3.oas.annotations.enums.SecuritySchemeType.HTTP,
