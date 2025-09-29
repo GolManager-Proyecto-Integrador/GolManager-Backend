5d426ca7401e29186cc3576e1ab7ac399352f6c6
mateo vargas <mateo.vargast@udea.edu.co>
2025-09-28 21:21:41 -0500

    DOCS: Documentación completa del endpoint de Authentication

--- src/main/java/co/golmanager/gestorweb/presentation/controller/AuthController.java
+++ src/main/java/co/golmanager/gestorweb/presentation/controller/AuthController.java
@@ -15,7 +15,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +30,68 @@ public class AuthController {
 
 
     @PostMapping("/register")
+    @Operation(
+            summary = "Register new user",
+            description = "Register a new user with the administrator profile.",
+            requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RegisterRequest.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Successful Login",
+                                            value = "{\"name\" :\"organizador1\" ,\"email\": \"organizador@torneos.com\",\"password\": \"organizador123\"}"
+                                    )
+                            }
+                    )
+            ),
+            responses = {@ApiResponse(
+                    responseCode = "200",
+                    description = "Succesful register",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = RegisterResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Successful Login",
+                                            value = "{\"id\":10,\"email\":\"organizador6@torneos.com\",\"name\":\"organizador6\",\"status\":200,\"token\":\"eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiVVNFUiIsInN1YiI6Im9yZ2FuaXphZG9yNkB0b3JuZW9zLmNvbSIsImlhdCI6MTc1OTEwOTU2MywiZXhwIjoxNzU5MTE2NzYzfQ.T5YVYGSBmZNg5wTJh_VjtPJURh3vIoOCTyxqFMQM7os\"}"
+                                    )
+                            }
+                    )
+            ), @ApiResponse(
+                    responseCode = "400",
+                    description = "Empty registration fields",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "Empty registration fields",
+                                            value = "{\"status\":400,\"error\":\"Validation Failed\",\"messages\":[\"name: The name cannot be empty.\",\"password: The password cannot be empty.\",\"email: The email cannot be empty.\"]}"
+                                    )
+                            }
+                    )
+            ), @ApiResponse(
+                    responseCode = "401",
+                    description = "A user without administrator permissions attempted to register a new user.",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = GeneralErrorResponse.class),
+                            examples = {
+                                    @ExampleObject(
+                                            name = "A user without permissions.",
+                                            value = "{\"path\":\"/error\",\"error\":\"Unauthorized\",\"message\":\"No autorizado o token inválido\",\"status\":401}"
+                                    )
+                            }
+                    )
+            )
 
+
+            }
+
+
+
+    )
     public ResponseEntity<RegisterResponse> register(@RequestBody @Valid RegisterRequest request, Authentication authentication) {
         return ResponseEntity.ok(authService.register(request, authentication.getName()));
 
@@ -41,7 +101,6 @@ public class AuthController {
     @Operation(
             summary = "Login User",
             description = "Authenticate a user and return the authentication token along wit user details.",
-            tags = "Authenticate",
             requestBody = @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     content = @Content(
                             mediaType = "application/json",
