f02ae7168851b892edf385ff3e344a9b4b357c1d
mateo vargas <mateo.vargast@udea.edu.co>
2025-11-22 23:49:51 -0500

    feature/HU06: añade el GET a el  endpoint /api/tournaments/matches/calendar

--- src/main/java/co/golmanager/gestorweb/presentation/controller/CalendarController.java
+++ src/main/java/co/golmanager/gestorweb/presentation/controller/CalendarController.java
@@ -1,17 +1,17 @@
 package co.golmanager.gestorweb.presentation.controller;
 
+import co.golmanager.gestorweb.presentation.dto.match.CreateMatchRequest;
+import co.golmanager.gestorweb.presentation.dto.match.CreateMatchResponse;
 import co.golmanager.gestorweb.presentation.dto.match.GetMatchResponse;
 import co.golmanager.gestorweb.service.interfaces.CalendarService;
+import co.golmanager.gestorweb.service.interfaces.MatchService;
+import co.golmanager.gestorweb.service.interfaces.PermissionEvaluatorService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.OffsetDateTime;
 import java.util.List;
@@ -24,9 +24,17 @@ import java.util.List;
 public class CalendarController {
 
     private final CalendarService calendarService;
+    private final PermissionEvaluatorService permissionEvaluatorService;
+    private final MatchService matchService;
 
     @GetMapping
     public ResponseEntity<List<GetMatchResponse>> getAllMatchesForOrganizer (@RequestParam OffsetDateTime initialDate, @RequestParam OffsetDateTime finishDate, Authentication authentication) {
         return ResponseEntity.ok(calendarService.getMatches(authentication, initialDate, finishDate));
+    }
+
+    @PostMapping
+    public ResponseEntity<CreateMatchResponse> createMatch(@RequestBody CreateMatchRequest createMatchRequest, Authentication authentication) {
+            permissionEvaluatorService.canAccessTournament(createMatchRequest.getTournamentId(), authentication);
+            return ResponseEntity.ok(matchService.createMatchResponse(createMatchRequest.getTournamentId(), createMatchRequest ,authentication.getName()));
     }
 }
